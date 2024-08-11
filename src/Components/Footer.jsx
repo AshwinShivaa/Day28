c938b8caad7597480bab787a7cd43023cab33217
AshwinShivaa <ashwinshivaaduraisamy@gmail.com>
2024-08-11 12:49:43 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+//Footer section as the copyright details of the webpage
+const Footer = () => {
+  return (
+    <>
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+            Copyright{" "}
+           Ashwin Shivaa{" "}
+            2024
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
